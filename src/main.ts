9eab2e9ee9a56f33210f0c2429b50ab2f20e9282
Sazzazaa <100590669+Sazzazaa@users.noreply.github.com>
2026-03-24 00:34:47 +0700

    Fix

--- src/main.ts
+++ src/main.ts
@@ -28,8 +28,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
-  
+await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
